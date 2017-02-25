3175f1db272b01c02410f99320116f5200b819dc
nileshrege <rege.nilesh@gmail.com>
2017-02-25 17:21:22 +0530

    refactored file name

--- src/main/java/com/saven/batch/writer/LineChartWriter.java
+++ src/main/java/com/saven/batch/writer/LineChartWriter.java
@@ -1,17 +1,15 @@
 package com.saven.batch.writer;
 
-import java.awt.*;
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.TickUnitSource;
+import org.jfree.chart.labels.StandardXYItemLabelGenerator;
+import org.jfree.chart.labels.XYItemLabelGenerator;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.general.Dataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -20,6 +18,12 @@ import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
+
+import java.awt.*;
+import java.io.File;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by nrege on 1/17/2017.
@@ -31,6 +35,10 @@ public class LineChartWriter implements ItemWriter , StepExecutionListener {
     String file;
 
     String chartName;
+
+    String xAxisName;
+
+    String yAxisName;
 
     @Override
     public void write(List list) throws Exception {
@@ -44,31 +52,11 @@ public class LineChartWriter implements ItemWriter , StepExecutionListener {
             dataset.addSeries(xySeries);
         }
 
-        final JFreeChart chart = ChartFactory.createXYLineChart(
-              getChartName(),
-              "X",
-              "Y",
-               dataset,
-               PlotOrientation.VERTICAL,
-              true,
-              true,
-              false
-        );
-
-        chart.setBackgroundPaint(Color.white);
-
-        final XYPlot plot = chart.getXYPlot();
-        plot.setBackgroundPaint(Color.white);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
-
-        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesLinesVisible(0, true);
-        renderer.setSeriesShapesVisible(1, true);
-        plot.setRenderer(renderer);
+        JFreeChart chart = createChart(dataset);
 
         int width = 640; /* Width of the image */
         int height = 480; /* Height of the image */
+
         File file = new File(this.file);
 
         ChartUtilities.saveChartAsJPEG(file ,chart, width ,height);
@@ -98,5 +86,77 @@ public class LineChartWriter implements ItemWriter , StepExecutionListener {
 
     public void setChartName(String chartName) {
         this.chartName = chartName;
+    }
+
+    public String getXAxisName() {
+        return xAxisName;
+    }
+
+    public void setXAxisName(String xAxisName) {
+        this.xAxisName = xAxisName;
+    }
+
+    public String getYAxisName() {
+        return yAxisName;
+    }
+
+    public void setYAxisName(String yAxisName) {
+        this.yAxisName = yAxisName;
+    }
+
+    private JFreeChart createChart(XYDataset dataset) {
+
+        // create the chart...
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                getChartName(), // chart title
+                getXAxisName(), // domain axis label
+                getYAxisName(), // range axis label
+                dataset,  // initial series
+                PlotOrientation.VERTICAL, // orientation
+                true, // include legend
+                true, // tooltips?
+                false // URLs?
+        );
+
+        // set chart background
+        chart.setBackgroundPaint(Color.white);
+
+        // set a few custom plot features
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(new Color(0xffffe0));
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.lightGray);
+        plot.setRangeGridlinePaint(Color.lightGray);
+
+        // set the plot's axes to display integers
+        TickUnitSource ticks = NumberAxis.createIntegerTickUnits();
+        NumberAxis domain = (NumberAxis) plot.getDomainAxis();
+        domain.setStandardTickUnits(ticks);
+        NumberAxis range = (NumberAxis) plot.getRangeAxis();
+        range.setStandardTickUnits(ticks);
+
+        // render shapes and lines
+        XYLineAndShapeRenderer renderer =
+                new XYLineAndShapeRenderer(true, true);
+        plot.setRenderer(renderer);
+        renderer.setBaseShapesVisible(true);
+        renderer.setBaseShapesFilled(true);
+
+        // set the renderer's stroke
+        Stroke stroke = new BasicStroke(
+                3f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL);
+        renderer.setBaseOutlineStroke(stroke);
+
+        // label the points
+        NumberFormat format = NumberFormat.getNumberInstance();
+        format.setMaximumFractionDigits(2);
+        XYItemLabelGenerator generator =
+                new StandardXYItemLabelGenerator(
+                        StandardXYItemLabelGenerator.DEFAULT_ITEM_LABEL_FORMAT,
+                        format, format);
+        renderer.setBaseItemLabelGenerator(generator);
+        renderer.setBaseItemLabelsVisible(true);
+
+        return chart;
     }
 }
